5adb0f47aed13e749291d7e51ad650ec7bc0fe0f
Ali Besharati <1368.ali.b@gmail.com>
2020-07-31 22:21:38 +0430

    Add project files.

---
+++ GetDataService/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace GetDataService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demo: Basic ODP.NET Core application to connect, query, and return
+            // results from an OracleDataReader to a console
+
+            //Create a connection to Oracle			
+            string conString = "User Id=ili;Password=iliqaz987;" +
+
+            //How to connect to an Oracle DB without SQL*Net configuration file
+            //  also known as tnsnames.ora.
+            "Data Source=86.104.46.204:1521/kanoondb;";
+
+            //How to connect to an Oracle DB with a DB alias.
+            //Uncomment below and comment above.
+            //"Data Source=;";
+
+            using (OracleConnection con = new OracleConnection(conString))
+            {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.BindByName = true;
+
+                        //Use the command to display employee names from 
+                        // the EMPLOYEES table
+                        cmd.CommandText = "select * from zaban.V_ili";
+
+                        
+
+                        //Execute the command and use DataReader to display the data
+                        OracleDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.WriteLine("Employee First Name: " + reader[i]);
+
+                            }
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Press 'Enter' to continue");
+
+                        reader.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
